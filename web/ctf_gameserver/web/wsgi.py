fca76d54d346520d215d7f969251b0ba1e00948c
Felix Dreissig <f30@f30.me>
2015-09-04 20:52:17 +0200

    Apply some coding styles
    Make the generated files match my personal style and remove
    inconsistent docstrings.

--- web/ctf_gameserver/web/wsgi.py
+++ web/ctf_gameserver/web/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for ctf_gameserver project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctf_gameserver.web.prod_settings")
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf_gameserver.web.prod_settings')
+
+# pylint: disable=invalid-name
 application = get_wsgi_application()
--- web/manage.py
+++ web/manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctf_gameserver.web.dev_settings")
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf_gameserver.web.dev_settings')
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
