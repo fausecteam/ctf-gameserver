b61665b6155849f691c9a5a72967673962c7c8e7
Christoph Egger <christoph@christoph-egger.org>
2015-11-19 17:01:16 +0100

    Add contest checker Baseclass

---
+++ checker/checker/contest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from checker.abstract import AbstractChecker
+
+import base64
+import sys
+import codecs
+
+class ContestChecker(AbstractChecker):
+    def __init__(self, tick, team, service, ip):
+        AbstractChecker.__init__(self, tick, team, service, ip)
+
+    def _rpc(self, function, *args):
+        sys.stdout.write("%s %s", function, " ".join(args))
+        sys.stdout.flush()
+        return sys.stdin.readline().strip()
+
+    def get_flag(self, tick, payload=None):
+        if payload is None:
+            return self._rpc("FLAG", str(tick))
+        else:
+            payload = codecs.encode(payload, 'hex').decode('latin-1')
+            return self._rpc("FLAG", str(tick), payload)
+
+    def store_blob(self, ident, blob):
+        data = base64.b64encode(blob)
+        return self._rpc("STORE", ident, data)
+
+    def retrieve_blob(self, ident):
+        data = self._rpc("RETRIEVE", ident)
+        return base64.b64decode(data)
