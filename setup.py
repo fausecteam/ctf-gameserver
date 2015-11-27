7d8a19bd0885848126c1ad0c333bb563521bb7a7
Christoph Egger <christoph@christoph-egger.org>
2015-11-27 18:02:30 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(name='CTF Gameserver',
+      version='0.1',
+      description='FAUST CTF Gameserver',
+      author='Christoph Egger',
+      author_email='Christoph.Egger@fau.de',
+      url='http://faustctf.net/',
+      packages=['ctf_gameserver.checker',
+                'ctf_gameserver.checker',
+                'ctf_gameserver.web',
+                'ctf_gameserver.web.scoring',
+                'ctf_gameserver.web.registration',
+                'ctf_gameserver.web.flatpages',
+                'ctf_gameserver.web.templatetags.templatetags',
+            ],
+      namespace_packages=['ctf_gameserver'],
+      package_dir = {'': 'src'},
+      license='MIT',
+)
