2945ae3bb8dd85bd96546cef4ff1e297774d7190
Christoph Egger <christoph@christoph-egger.org>
2015-12-09 21:52:09 +0100

    Add forward/reverse mapping of checkerstati

--- checker/checker/__init__.py
+++ checker/checker/__init__.py
@@ -7,3 +7,11 @@ OK = 0
 TIMEOUT = 1
 NOTWORKING = 2
 NOTFOUND = 3
+
+_mapping = ["OK", "TIMEOUT", "NOTWORKING", "NOTFOUND"]
+
+def string_to_result(strresult):
+    return _mapping.index(strresult)
+
+def result_to_string(result):
+    return _mapping[result]
