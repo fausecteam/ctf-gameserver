5f09dcde0a9360d287d4f86feccc1749f08741d6
Christoph Egger <christoph@christoph-egger.org>
2015-12-26 13:03:49 +0100

    Make constants properly

--- checker/ctf_gameserver/checker/__init__.py
+++ checker/ctf_gameserver/checker/__init__.py
@@ -3,16 +3,4 @@
 from .local import LocalChecker as BaseChecker
 #from .contest import ContestChecker as BaseChecker
 
-OK = 0
-TIMEOUT = 1
-NOTWORKING = 2
-NOTFOUND = 3
-RECOVERING = 4
-
-_mapping = ["OK", "TIMEOUT", "NOTWORKING", "NOTFOUND", "RECOVERING"]
-
-def string_to_result(strresult):
-    return _mapping.index(strresult)
-
-def result_to_string(result):
-    return _mapping[result]
+from .constants import *
