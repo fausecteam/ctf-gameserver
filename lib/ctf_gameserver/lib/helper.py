c568cf4b1be5e38b92f7d3a9131e67ff9eff764e
Felix Dreissig <f30@f30.me>
2018-05-30 15:41:48 +0200

    Improve config argument splitting to allow quoted spaces

--- lib/ctf_gameserver/lib/helper.py
+++ lib/ctf_gameserver/lib/helper.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import shlex
+
 def convert_arg_line_to_args(arg_line):
     """argparse helper for splitting input from config
 
     Allows comment lines in configfiles and allows both argument and
     value on the same line
     """
-    if arg_line.strip().startswith('#'):
-        return []
-    else:
-        return arg_line.split()
+    return shlex.split(arg_line, comments=True)
