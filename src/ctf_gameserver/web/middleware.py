d36a548f412db2d535ae802b74d2f75326ef6e0f
Felix Dreissig <f30@f30.me>
2019-04-14 21:54:21 +0200

    Web: Resolve REVISIT comments for current Django version

--- src/ctf_gameserver/web/middleware.py
+++ src/ctf_gameserver/web/middleware.py
@@ -1,4 +1,3 @@
-from django.core.urlresolvers import reverse
 from django.conf import settings
 
 
@@ -8,12 +7,8 @@ class CSPMiddlware:
     HTTP response.
     """
 
-    def process_response(self, request, response):
+    def process_response(self, _, response):
         if settings.CSP_POLICIES:
-            # REVISIT: This shall be removed when https://code.djangoproject.com/ticket/25165 is fixed
-            if request.path_info.startswith(reverse('admin:index')):
-                return response
-
             policies = []
 
             for directive, values in settings.CSP_POLICIES.items():
--- src/ctf_gameserver/web/registration/util.py
+++ src/ctf_gameserver/web/registration/util.py
@@ -2,8 +2,6 @@ import os
 import locale
 import csv
 
-from django.utils.http import int_to_base36
-from django.utils.crypto import salted_hmac
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
@@ -13,23 +11,16 @@ class EmailConfirmationTokenGenerator(PasswordResetTokenGenerator):
     tokens.
     """
 
-    # REVISIT: For Django > 1.8, the parent class has been refactored "to be a bit more extensible"
-    def _make_token_with_timestamp(self, user, timestamp):
+    key_salt = 'ctf_gameserver.web.registration.util.EmailConfirmationTokenGenerator'
+
+    def _make_hash_value(self, user, timestamp):
         """
         This is mostly a copy of the parent class' method.
-        The salt has been changed and instead of the password, the user's email address is included in the
-        hash.
+        Instead of the password, the user's email address is included in the hash.
         """
-        ts_b36 = int_to_base36(timestamp)
-
-        key_salt = 'ctf_gameserver.web.registration.util.EmailConfirmationTokenGenerator'
-
         login_timestamp = '' if user.last_login is None else user.last_login.replace(microsecond=0,
                                                                                      tzinfo=None)
-        value = (str(user.pk) + user.email + str(login_timestamp) + str(timestamp))
-        hash_value = salted_hmac(key_salt, value).hexdigest()[::2]
-
-        return '%s-%s' % (ts_b36, hash_value)
+        return str(user.pk) + user.email + str(login_timestamp) + str(timestamp)
 
 
 email_token_generator = EmailConfirmationTokenGenerator()    # pylint: disable=invalid-name
