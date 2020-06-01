ad32b12b1f3419dd96dea644c15515eb34e3a7da
Felix Dreißig <f30@f30.me>
2020-06-01 13:20:45 +0200

    Clean up "flag" lib code

--- src/ctf_gameserver/lib/flag.py
+++ src/ctf_gameserver/lib/flag.py
@@ -7,94 +7,107 @@ import zlib
 
 from .Keccak import Keccak
 
-# length of the MAC (in bits)
-MACLEN = 80
-# length of the Payload (in bytes)
-PAYLOADLEN = 8
-# flaggenprefix
+# Length of the MAC (in bits)
+MAC_LEN = 80
+# Length of the payload (in bytes)
+PAYLOAD_LEN = 8
+# timestamp + team + service + payload
+DATA_LEN = 4 + 1 + 1 + PAYLOAD_LEN
+# Flag prefix
 PREFIX = "FAUST"
-# gültigkeit in sekunden
+# Flag validity in seconds
 VALID = 900
 
 keccak = Keccak(100)
-# timestamp + team + service + payload
-datalength = 4 + 1 + 1 + PAYLOADLEN
+
 
 def generate(team_id, service_id, secret, payload=None, timestamp=None):
-    """Generates a new flag
-
-    Arguments:
-     - team: Team that should protect this flag
-     - service: Service this flag belogns to
-     - payload: data to secure in the flag, defaults to
-       crc32(timestamp, team, service), zero padded
-     - timestamp: timestamp when to expire validity of the
-       flags. Defaults to 15 minutes in the future
     """
+    Generates a flag for the given arguments. This is deterministic and should always return the same
+    result for the same arguments (and the same time, if no timestamp is explicitly specified).
+
+    Args:
+        team_id: ID of the team protecting this flag
+        service_id: ID of the service this flag belongs to
+        payload: 8 bytes of data to store in the flag, defaults to zero-padded
+                 CRC32(timestamp, team, service)
+        timestamp: Timestamp at which the flag expires, defaults to 15 minutes in the future
+    """
+
     if timestamp is None:
         timestamp = time.time() + VALID
 
-    protecteddata = struct.pack("!i c c", int(timestamp),
-                                bytes([team_id]), bytes([service_id]))
+    protected_data = struct.pack("!i c c", int(timestamp), bytes([team_id]), bytes([service_id]))
 
     if payload is None:
-        payload = struct.pack("!I I", zlib.crc32(protecteddata), 0)
+        payload = struct.pack("!I I", zlib.crc32(protected_data), 0)
 
-    protecteddata = protecteddata + payload
+    protected_data += payload
 
-    mac = codecs.encode(secret, 'hex') + codecs.encode(protecteddata, 'hex')
-    # this is not sha1 / sha2, macs are that simple!
-    mac = keccak.Keccak(((len(secret) + len(protecteddata))*8,
-                         mac.decode('latin-1')),
-                        n=MACLEN)
+    # Keccak does not need an HMAC construction, the secret can simply be prepended
+    protected_hex = codecs.encode(secret, 'hex') + codecs.encode(protected_data, 'hex')
+    mac_hex = keccak.Keccak(((len(secret) + len(protected_data))*8, protected_hex.decode('ascii')),
+                            n=MAC_LEN)
+    mac = codecs.decode(mac_hex, 'hex')
 
-    mac = codecs.decode(mac, 'hex')
+    return PREFIX + '_' + base64.b64encode(protected_data + mac).decode('ascii')
 
-    return "%s_%s" % (PREFIX, base64.b64encode(protecteddata + mac).decode('latin-1'))
-
-class FlagVerificationError(Exception):
-    "Baseclass for all Flag Exceptions"
-    pass
-
-class InvalidFlagFormat(FlagVerificationError):
-    "Flag does not match the regex or does not b64decode properly"
-    pass
-
-class InvalidFlagMAC(FlagVerificationError):
-    "MAC does not match with configured secret"
-    pass
-
-class FlagExpired(FlagVerificationError):
-    "Flag is already expired"
-    pass
 
 def verify(flag, secret):
-    """ Returns the data encoded within the flag:
-        (team, service, payload, timestamp)
-
-        May raise an appriproate exception if decoding fails
     """
-    if not flag.startswith(PREFIX+"_"):
-        raise InvalidFlagFormat("Flag is not in the expected format")
+    Verfies flag validity and returns data from the flag.
+    Will raise an appropriate exception if verification fails.
+
+    Returns:
+        Data from the flag as a tuple of (team, service, payload, timestamp)
+    """
+
+    if not flag.startswith(PREFIX + '_'):
+        raise InvalidFlagFormat()
 
     try:
-        rawdata = base64.b64decode(flag.split('_')[1])
+        raw_flag = base64.b64decode(flag.split('_')[1])
     except binascii.Error:
-        raise InvalidFlagFormat("Flag is not in the expected format")
+        raise InvalidFlagFormat()
 
-    protecteddata, mac = rawdata[:datalength], rawdata[datalength:]
-    computedmac = codecs.encode(secret, 'hex') + codecs.encode(protecteddata, 'hex')
-    computedmac = keccak.Keccak(((len(secret) + len(protecteddata))*8,
-                                 computedmac.decode('latin-1')),
-                                n=MACLEN)
+    protected_data, flag_mac = raw_flag[:DATA_LEN], raw_flag[DATA_LEN:]
+    protected_hex = codecs.encode(secret, 'hex') + codecs.encode(protected_data, 'hex')
+    mac_hex = keccak.Keccak(((len(secret) + len(protected_data))*8, protected_hex.decode('ascii')),
+                            n=MAC_LEN)
+    mac = codecs.decode(mac_hex, 'hex')
 
-    computedmac = codecs.decode(computedmac, 'hex')
-    if not computedmac == mac:
-        raise InvalidFlagMAC("MAC does not match")
+    if not mac == flag_mac:
+        raise InvalidFlagMAC()
 
-    timestamp, team, service = struct.unpack("!i c c", protecteddata[:6])
-    payload = protecteddata[6:]
+    timestamp, team, service = struct.unpack("!i c c", protected_data[:6])
+    payload = protected_data[6:]
     if time.time() - timestamp > 0:
         raise FlagExpired(time.time() - timestamp)
 
-    return int.from_bytes(team, 'big'), int.from_bytes(service, 'big'), payload, timestamp
+    return (int.from_bytes(team, 'big'), int.from_bytes(service, 'big'), payload, timestamp)
+
+
+class FlagVerificationError(Exception):
+    """
+    Base class for all Flag Exceptions.
+    """
+
+
+class InvalidFlagFormat(FlagVerificationError):
+    """
+    Flag does not match the expected format.
+    """
+
+
+class InvalidFlagMAC(FlagVerificationError):
+    """
+    MAC does not match with configured secret.
+    """
+
+
+class FlagExpired(FlagVerificationError):
+    """
+    Flag is already expired.
+    """
+
+    pass
