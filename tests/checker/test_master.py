fausecteam/ctf-gameserver

commit babf7a1c892b4fd1e9c33d9255da8872a25be7c6

Major rewrite of everything related to Checkers
* New API and library for Checker Scripts
* Build foundation for Checker Scripts in other languages (IPC
  protocol)
* Checker Scripts can be executed locally (during development) without
  a dedicated Runner
* No more Checker Slaves
* Launch processes using Python multiprocessing instead of asyncio
* Scheduling of checks within a tick is different, should lead to more
  even distribution
* Run Checker Scripts as different users than the Checker Master
* Lots of test cases

Closes: https://github.com/fausecteam/ctf-gameserver/issues/1
Closes: https://github.com/fausecteam/ctf-gameserver/issues/20
Closes: https://github.com/fausecteam/ctf-gameserver/issues/33
Closes: https://github.com/fausecteam/ctf-gameserver/issues/38
Improves: https://github.com/fausecteam/ctf-gameserver/issues/17
Improves: https://github.com/fausecteam/ctf-gameserver/issues/27
Improves: https://github.com/fausecteam/ctf-gameserver/issues/29
Improves: https://github.com/fausecteam/ctf-gameserver/issues/32
Improves: https://github.com/fausecteam/ctf-gameserver/issues/35

src/ctf_gameserver/checker/database.py
@@ -1,0 +1,138 @@
+from ctf_gameserver.lib.database import transaction_cursor
+from ctf_gameserver.lib.exceptions import DBDataError
+
+
+def get_control_info(db_conn, prohibit_changes=False):
+    """
+    Returns a dictionary containing relevant information about the competion, as stored in the game database.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('SELECT valid_ticks, tick_duration FROM scoring_gamecontrol')
+        result = cursor.fetchone()
+
+    if result is None:
+        raise DBDataError('Game control information has not been configured')
+
+    return {
+        'valid_ticks': result[0],
+        'tick_duration': result[1]
+    }
+
+
+def get_service_attributes(db_conn, service_slug, prohibit_changes=False):
+    """
+    Returns ID and name of a service for a given slug.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('SELECT id, name FROM scoring_service WHERE slug = %s', (service_slug,))
+        result = cursor.fetchone()
+
+    return {
+        'id': result[0],
+        'name': result[1]
+    }
+
+
+def get_current_tick(db_conn, prohibit_changes=False):
+    """
+    Reads the current tick from the game database.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('SELECT current_tick FROM scoring_gamecontrol')
+        result = cursor.fetchone()
+
+    return result[0]
+
+
+def get_task_count(db_conn, service_id, prohibit_changes=False):
+    """
+    Returns the total number of tasks for the given service in the current tick.
+    With our current Controller implementation, this should always be equal to the number of teams.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('SELECT COUNT(*)'
+                       '    FROM scoring_flag flag, scoring_gamecontrol control'
+                       '    WHERE flag.tick = control.current_tick'
+                       '        AND flag.service_id = %s', (service_id,))
+        result = cursor.fetchone()
+
+    return result[0]
+
+
+def get_new_tasks(db_conn, service_id, task_count, prohibit_changes=False):
+    """
+    Retrieves the given number of random open check tasks and marks them as in progress.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('SELECT flag.id, flag.protecting_team_id, flag.tick'
+                       '    FROM scoring_flag flag, scoring_gamecontrol control'
+                       '    WHERE flag.placement_start is NULL'
+                       '        AND flag.tick = control.current_tick'
+                       '        AND flag.service_id = %s'
+                       '    ORDER BY RANDOM()'
+                       '    LIMIT %s'
+                       '    FOR UPDATE OF flag', (service_id, task_count))
+        tasks = cursor.fetchall()
+
+        # Mark placement as in progress
+        cursor.executemany('UPDATE scoring_flag'
+                           '    SET placement_start = NOW()'
+                           '    WHERE id = %s', [(task[0],) for task in tasks])
+
+    return [{
+        'team_id': task[1],
+        'tick': task[2]
+    } for task in tasks]
+
+
+def commit_result(db_conn, service_id, team_id, tick, result, prohibit_changes=False):
+    """
+    Saves the result from a Checker run to game database.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('INSERT INTO scoring_statuscheck'
+                       '    (service_id, team_id, tick, status, timestamp)'
+                       '    VALUES (%s, %s, %s, %s, NOW())', (service_id, team_id, tick, result))
+        # (In case of `prohibit_changes`,) PostgreSQL checks the database grants even if nothing is matched
+        # by `WHERE`
+        cursor.execute('UPDATE scoring_flag'
+                       '    SET placement_end = NOW()'
+                       '    WHERE service_id = %s AND protecting_team_id = %s AND tick = %s', (service_id,
+                                                                                               team_id,
+                                                                                               tick))
+
+
+def load_state(db_conn, service_id, team_id, identifier, prohibit_changes=False):
+    """
+    Loads Checker data from state database.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('SELECT data FROM checkerstate'
+                       '    WHERE service_id = %s'
+                       '        AND team_id = %s'
+                       '        AND identifier = %s', (service_id, team_id, identifier))
+        data = cursor.fetchone()
+
+    if data is None:
+        return None
+    return data[0]
+
+
+def store_state(db_conn, service_id, team_id, identifier, data, prohibit_changes=False):
+    """
+    Stores Checker data in state database.
+    """
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        # (In case of `prohibit_changes`,) PostgreSQL checks the database grants even if no CONFLICT occurs
+        cursor.execute('INSERT INTO checkerstate (service_id, team_id, identifier, data)'
+                       '    VALUES (%s, %s, %s, %s)'
+                       '    ON CONFLICT (service_id, team_id, identifier)'
+                       '        DO UPDATE SET data = EXCLUDED.data', (service_id, team_id, identifier, data))

commit 1bf14741fb695ea7b8ac7a60c977b3ab0038f6fe

Checker: fix broken flag generation
handle_flag_request() was ignoring the tick argument generating only
flags for the current tick. This was totally broken.

The integration tests (tests/checker/test_integration.py) did not catch
this because they never compared the generated flags.

Broken in babf7a1 ("Major rewrite of everything related to Checkers",
2020-05-31)

src/ctf_gameserver/checker/database.py
@@ -8,15 +8,16 @@ def get_control_info(db_conn, prohibit_changes=False):
     """
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
-        cursor.execute('SELECT valid_ticks, tick_duration FROM scoring_gamecontrol')
+        cursor.execute('SELECT start, valid_ticks, tick_duration FROM scoring_gamecontrol')
         result = cursor.fetchone()
 
     if result is None:
         raise DBDataError('Game control information has not been configured')
 
     return {
-        'valid_ticks': result[0],
-        'tick_duration': result[1]
+        'contest_start': result[0],
+        'valid_ticks': result[1],
+        'tick_duration': result[2]
     }
 
 

src/ctf_gameserver/checker/master.py
@@ -208,6 +208,7 @@ class MasterLoop:
 
     def refresh_control_info(self):
         control_info = database.get_control_info(self.game_db_conn)
+        self.contest_start = control_info['contest_start']
         self.tick_duration = datetime.timedelta(seconds=control_info['tick_duration'])
         self.flag_valid_ticks = control_info['valid_ticks']
 
@@ -259,14 +260,22 @@ class MasterLoop:
 
     def handle_flag_request(self, task_info, params):
         try:
+            tick = int(params['tick'])
+        except (KeyError, ValueError):
+            return None
+
+        try:
             payload = base64.b64decode(params['payload'])
         except KeyError:
             payload = None
 
         if payload == b'':
             payload = None
-        expiration = datetime.datetime.utcnow() + (self.tick_duration * self.flag_valid_ticks)
 
+        # We need current value for self.contest_start which might have changed
+        self.refresh_control_info()
+
+        expiration = self.contest_start + (self.flag_valid_ticks + tick) * self.tick_duration
         return flag_lib.generate(task_info['team'], self.service['id'], self.flag_secret, payload,
                                  expiration.timestamp())
 

tests/checker/test_integration.py
@@ -45,6 +45,7 @@ class IntegrationTest(DatabaseTestCase):
 
         # Start tick
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')
@@ -86,6 +87,7 @@ class IntegrationTest(DatabaseTestCase):
                                  90, 1, 10, '0.0.%s.1', b'secret', {})
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')
@@ -115,6 +117,7 @@ class IntegrationTest(DatabaseTestCase):
                                  90, 1, 10, '0.0.%s.1', b'secret', {})
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')
@@ -144,6 +147,7 @@ class IntegrationTest(DatabaseTestCase):
                                  90, 1, 10, '0.0.%s.1', b'secret', {})
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')
@@ -179,6 +183,7 @@ class IntegrationTest(DatabaseTestCase):
                                  90, 1, 10, '0.0.%s.1', b'secret', {})
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')
@@ -238,6 +243,7 @@ class IntegrationTest(DatabaseTestCase):
 
         # Tick 0
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             # Also add flags for service 2 (which does not get checked) to make sure it won't get touched
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
@@ -335,6 +341,7 @@ class IntegrationTest(DatabaseTestCase):
 
         # Tick 0
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0), (1, 3, 0)')
@@ -402,6 +409,7 @@ class IntegrationTest(DatabaseTestCase):
                                  90, 1, 10, '0.0.%s.1', b'secret', {})
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')
@@ -430,6 +438,7 @@ class IntegrationTest(DatabaseTestCase):
                                  'ctf-checkerrunner', 90, 1, 10, '0.0.%s.1', b'secret', {})
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')
@@ -469,6 +478,7 @@ class IntegrationTest(DatabaseTestCase):
                                  'ctf-checkerrunner', 90, 1, 10, '0.0.%s.1', b'secret', {})
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
             cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
             cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                            '    VALUES (1, 2, 0)')

tests/checker/test_master.py
@@ -15,9 +15,9 @@ class MasterTest(DatabaseTestCase):
         self.master_loop = MasterLoop(self.connection, None, 'service1', '/dev/null', None, 90, 8, 10,
                                       '0.0.%s.1', b'secret', {})
 
-    @patch('datetime.datetime')
-    def test_handle_flag_request(self, datetime_mock):
-        datetime_mock.utcnow.return_value = datetime.datetime.utcfromtimestamp(42)
+    def test_handle_flag_request(self):
+        with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
 
         task_info = {
             'service': 'Service 1',
@@ -25,15 +25,35 @@ class MasterTest(DatabaseTestCase):
             'tick': 1
         }
 
-        params1 = {}
+        params1 = {'tick': 1}
         resp1 = self.master_loop.handle_flag_request(task_info, params1)
-        params2 = {}
+        params2 = {'tick': 1}
         resp2 = self.master_loop.handle_flag_request(task_info, params2)
-        params3 = {'payload': 'TmV2ZXIgZ28='}
+        params3 = {'tick': 1, 'payload': 'TmV2ZXIgZ28='}
         resp3 = self.master_loop.handle_flag_request(task_info, params3)
 
         self.assertEqual(resp1, resp2)
         self.assertNotEqual(resp1, resp3)
+
+        params4 = {'tick': 2}
+        resp4 = self.master_loop.handle_flag_request(task_info, params4)
+        params5 = {'tick': 2}
+        resp5 = self.master_loop.handle_flag_request(task_info, params5)
+
+        self.assertEqual(resp4, resp5)
+        self.assertNotEqual(resp1, resp4)
+
+        params6 = {}
+        self.assertIsNone(self.master_loop.handle_flag_request(task_info, params6))
+
+        # Changing the start time changes all flags
+        with transaction_cursor(self.connection) as cursor:
+            # SQLite syntax for tests
+            cursor.execute('UPDATE scoring_gamecontrol SET start=DATETIME("now", "+1 hour")')
+        resp1_again = self.master_loop.handle_flag_request(task_info, params1)
+        resp4_again = self.master_loop.handle_flag_request(task_info, params4)
+        self.assertNotEqual(resp1, resp1_again)
+        self.assertNotEqual(resp4, resp4_again)
 
     def test_handle_result_request(self):
         task_info = {