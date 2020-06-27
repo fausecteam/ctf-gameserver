fausecteam/ctf-gameserver

commit e2fabb54fb08fd484fe1479d92d2aeea562ba25b

Introduce net numbers to make teams more anonymous
The goal is to make it hard for teams to determine which IP net belongs
to which team. This way they cannot attack or not attack specific
(stronger) teams.

Remove the team id from the (public) website to prevent false
conclusions.

src/ctf_gameserver/submission/flagserver.py
@@ -30,7 +30,7 @@ class FlagHandler(asynchat.async_chat):
         self._dbconnection = dbconnection
         self._secret = base64.b64decode(secret)
         self.buffer = b''
-        self._logger.info("Accepted connection from Team %s", self.team)
+        self._logger.info("Accepted connection from Team (Net Number) %s", self.team)
         self._banner()
         self._conteststart = conteststart
         self._contestend = contestend
@@ -102,6 +102,14 @@ class FlagHandler(asynchat.async_chat):
     def _store_capture(self, team, service, timestamp):
         with self._dbconnection:
             with self._dbconnection.cursor() as cursor:
+                cursor.execute("""SELECT user_id FROM registration_team WHERE net_number = %s""",
+                               (team,))
+                data = cursor.fetchone()
+                if data is None:
+                    self._reply(u"Unknown team net".encode("utf-8"))
+                    return False
+                team = data[0]
+
                 cursor.execute("""SELECT nop_team FROM registration_team WHERE user_id = %s""",
                                (team,))
                 nopp, = cursor.fetchone()

go/checkerlib/lib.go
@@ -240,13 +240,13 @@ func LoadState(key string) interface{} {
 // Must be called by all Checker Scripts.
 func RunCheck(checker Checker) {
 	if len(os.Args) != 4 {
-		log.Fatalf("usage: %s <ip> <team> <tick>", os.Args[0])
+		log.Fatalf("usage: %s <ip> <team-net-no> <tick>", os.Args[0])
 	}
 
 	ip := os.Args[1]
 	team, err := strconv.Atoi(os.Args[2])
 	if err != nil {
-		log.Fatalf("invalid team id %s", os.Args[2])
+		log.Fatalf("invalid team net number %s", os.Args[2])
 	}
 	tick, err := strconv.Atoi(os.Args[3])
 	if err != nil {

scripts/controller/ctf-flagid
@@ -33,6 +33,8 @@ def clean_dict(result):
     return result
 
 
+# TODO: adapt to IPv6
+# TODO: adapt to net_number
 def update_flagids(db, service_names, fname):
     result = dict()
     with db:

src/ctf_gameserver/checker/database.py
@@ -1,3 +1,5 @@
+import logging
+
 from ctf_gameserver.lib.database import transaction_cursor
 from ctf_gameserver.lib.exceptions import DBDataError
 
@@ -76,11 +78,12 @@ def get_new_tasks(db_conn, service_id, task_count, prohibit_changes=False):
     """
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
-        cursor.execute('SELECT flag.id, flag.protecting_team_id, flag.tick'
-                       '    FROM scoring_flag flag, scoring_gamecontrol control'
+        cursor.execute('SELECT flag.id, flag.protecting_team_id, flag.tick, team.net_number'
+                       '    FROM scoring_flag flag, scoring_gamecontrol control, registration_team team'
                        '    WHERE flag.placement_start is NULL'
                        '        AND flag.tick = control.current_tick'
                        '        AND flag.service_id = %s'
+                       '        AND flag.protecting_team_id = team.user_id'
                        '    ORDER BY RANDOM()'
                        '    LIMIT %s'
                        '    FOR UPDATE OF flag', (service_id, task_count))
@@ -93,16 +96,27 @@ def get_new_tasks(db_conn, service_id, task_count, prohibit_changes=False):
 
     return [{
         'team_id': task[1],
+        'team_net_no': task[3],
         'tick': task[2]
     } for task in tasks]
 
 
-def commit_result(db_conn, service_id, team_id, tick, result, prohibit_changes=False):
+def commit_result(db_conn, service_id, team_net_no, tick, result, prohibit_changes=False, fake_team_id=None):
     """
     Saves the result from a Checker run to game database.
     """
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('SELECT user_id FROM registration_team'
+                       '    WHERE net_number = %s', (team_net_no,))
+        data = cursor.fetchone()
+        if data is None:
+            if fake_team_id is None:
+                logging.error('No team found with net number %d, cannot commit result', team_net_no)
+                return
+            data = (fake_team_id,)
+        team_id = data[0]
+
         cursor.execute('INSERT INTO scoring_statuscheck'
                        '    (service_id, team_id, tick, status, timestamp)'
                        '    VALUES (%s, %s, %s, %s, NOW())', (service_id, team_id, tick, result))
@@ -115,7 +129,7 @@ def commit_result(db_conn, service_id, team_id, tick, result, prohibit_changes=F
                                                                                                tick))
 
 
-def load_state(db_conn, service_id, team_id, identifier, prohibit_changes=False):
+def load_state(db_conn, service_id, team_net_no, identifier, prohibit_changes=False):
     """
     Loads Checker data from state database.
     """
@@ -123,8 +137,8 @@ def load_state(db_conn, service_id, team_id, identifier, prohibit_changes=False)
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
         cursor.execute('SELECT data FROM checkerstate'
                        '    WHERE service_id = %s'
-                       '        AND team_id = %s'
-                       '        AND identifier = %s', (service_id, team_id, identifier))
+                       '        AND team_net_no = %s'
+                       '        AND identifier = %s', (service_id, team_net_no, identifier))
         data = cursor.fetchone()
 
     if data is None:
@@ -132,14 +146,15 @@ def load_state(db_conn, service_id, team_id, identifier, prohibit_changes=False)
     return data[0]
 
 
-def store_state(db_conn, service_id, team_id, identifier, data, prohibit_changes=False):
+def store_state(db_conn, service_id, team_net_no, identifier, data, prohibit_changes=False):
     """
     Stores Checker data in state database.
     """
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
         # (In case of `prohibit_changes`,) PostgreSQL checks the database grants even if no CONFLICT occurs
-        cursor.execute('INSERT INTO checkerstate (service_id, team_id, identifier, data)'
+        cursor.execute('INSERT INTO checkerstate (service_id, team_net_no, identifier, data)'
                        '    VALUES (%s, %s, %s, %s)'
-                       '    ON CONFLICT (service_id, team_id, identifier)'
-                       '        DO UPDATE SET data = EXCLUDED.data', (service_id, team_id, identifier, data))
+                       '    ON CONFLICT (service_id, team_net_no, identifier)'
+                       '        DO UPDATE SET data = EXCLUDED.data', (service_id, team_net_no, identifier,
+                                                                      data))

src/ctf_gameserver/checker/master.py
@@ -139,7 +139,7 @@ def main():
 
         database.get_task_count(game_db_conn, service_id, prohibit_changes=True)
         database.get_new_tasks(game_db_conn, service_id, 1, prohibit_changes=True)
-        database.commit_result(game_db_conn, service_id, 1, 0, 0, prohibit_changes=True)
+        database.commit_result(game_db_conn, service_id, 1, 0, 0, prohibit_changes=True, fake_team_id=1)
         database.load_state(state_db_conn, service_id, 1, 'identifier', prohibit_changes=True)
         database.store_state(state_db_conn, service_id, 1, 'identifier', 'data', prohibit_changes=True)
     except psycopg2.ProgrammingError as e:
@@ -236,15 +236,16 @@ class MasterLoop:
                 elif req['action'] == ACTION_RESULT:
                     self.handle_result_request(req['info'], req['param'])
                 else:
-                    logging.error('Unknown action received from Checker Script for team %d in tick %d: %s',
-                                  req['info']['team'], req['info']['tick'], req['action'])
+                    logging.error('Unknown action received from Checker Script for team %d (net number %d) '
+                                  'in tick %d: %s', req['info']['_team_id'], req['info']['team'],
+                                  req['info']['tick'], req['action'])
                     # We can't signal an error to the Checker Script (which might be waiting for a response),
                     # so our only option is to kill it
                     self.supervisor.terminate_runner(req['runner_id'])
                     send_resp = False
             except:    # noqa, pylint: disable=bare-except
-                logging.exception('Checker Script communication error for team %d in tick %d:',
-                                  req['info']['team'], req['info']['tick'])
+                logging.exception('Checker Script communication error for team %d (net number %d) in tick '
+                                  '%d:', req['info']['_team_id'], req['info']['team'], req['info']['tick'])
                 self.supervisor.terminate_runner(req['runner_id'])
             else:
                 if send_resp:
@@ -290,19 +291,19 @@ class MasterLoop:
         try:
             result = int(param)
         except ValueError:
-            logging.error('Invalid result from Checker Script for team %d in tick %d: %s',
-                          task_info['team'], task_info['tick'], param)
+            logging.error('Invalid result from Checker Script for team %d (net number %d) in tick %d: %s',
+                          task_info['_team_id'], task_info['team'], task_info['tick'], param)
             return
 
         try:
             check_result = CheckResult(result)
         except ValueError:
-            logging.error('Invalid result from Checker Script for team %d in tick %d: %d',
-                          task_info['team'], task_info['tick'], result)
+            logging.error('Invalid result from Checker Script for team %d (net number %d) in tick %d: %d',
+                          task_info['_team_id'], task_info['team'], task_info['tick'], result)
             return
 
-        logging.info('Result from Checker Script for team %d in tick %d: %s', task_info['team'],
-                     task_info['tick'], check_result)
+        logging.info('Result from Checker Script for team %d (net number %d) in tick %d: %s',
+                     task_info['_team_id'], task_info['team'], task_info['tick'], check_result)
         database.commit_result(self.game_db_conn, self.service['id'], task_info['team'], task_info['tick'],
                                result)
 
@@ -320,15 +321,17 @@ class MasterLoop:
 
         tasks = database.get_new_tasks(self.game_db_conn, self.service['id'], self.tasks_per_launch)
         for task in tasks:
-            ip = self.ip_pattern % task['team_id']
-            runner_args = [self.checker_script, ip, str(task['team_id']), str(task['tick'])]
+            ip = self.ip_pattern % task['team_net_no']
+            runner_args = [self.checker_script, ip, str(task['team_net_no']), str(task['tick'])]
 
             # Information in task_info should be somewhat human-readable, because it also ends up in Checker
             # Script logs
             task_info = {'service': self.service['name'],
-                         'team': task['team_id'],
+                         'team': task['team_net_no'],
+                         '_team_id': task['team_id'],
                          'tick': current_tick}
-            logging.info('Starting Checker Script for team %d in tick %d', task['team_id'], current_tick)
+            logging.info('Starting Checker Script for team %d (net number %d) in tick %d', task['team_id'],
+                         task['team_net_no'], current_tick)
             self.supervisor.start_runner(runner_args, self.sudo_user, task_info, self.logging_params)
 
     def update_launch_params(self):

src/ctf_gameserver/checker/supervisor.py
@@ -153,6 +153,7 @@ def _run_checker_script(args, sudo_user, info, logging_params, runner_id, queue_
         gelf_class = graypy.GELFHandler
         if ':' in logging_params['gelf']['host']:
             import socket
+
             class IPv6GELFHandler(graypy.GELFHandler):
                 def makeSocket(self):
                     return socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)

src/ctf_gameserver/checkerlib/lib.py
@@ -95,7 +95,7 @@ class BaseChecker:
 
     Attributes:
         ip: Vulnbox IP address of the team to be checked
-        team: ID of the team to be checked
+        team: Net number of the team to be checked
     """
 
     def __init__(self, ip: str, team: int) -> None:
@@ -190,7 +190,7 @@ def run_check(checker_cls: Type[BaseChecker]) -> None:
     """
 
     if len(sys.argv) != 4:
-        raise Exception('Invalid arguments, usage: {} <ip> <team> <tick>'.format(sys.argv[0]))
+        raise Exception('Invalid arguments, usage: {} <ip> <team-net-no> <tick>'.format(sys.argv[0]))
 
     ip = sys.argv[1]
     team = int(sys.argv[2])

src/ctf_gameserver/lib/flag.py
@@ -18,13 +18,13 @@ PREFIX = "FAUST"
 VALID = 900
 
 
-def generate(team_id, service_id, secret, payload=None, timestamp=None):
+def generate(team_net_no, service_id, secret, payload=None, timestamp=None):
     """
     Generates a flag for the given arguments. This is deterministic and should always return the same
     result for the same arguments (and the same time, if no timestamp is explicitly specified).
 
     Args:
-        team_id: ID of the team protecting this flag
+        team_net_no: Net number of the team protecting this flag
         service_id: ID of the service this flag belongs to
         payload: 8 bytes of data to store in the flag, defaults to zero-padded
                  CRC32(timestamp, team, service)
@@ -34,9 +34,9 @@ def generate(team_id, service_id, secret, payload=None, timestamp=None):
     if timestamp is None:
         timestamp = time.time() + VALID
 
-    if team_id > 65535:
-        raise ValueError('Team ID must fit in 16 bits')
-    protected_data = struct.pack("!i H c", int(timestamp), team_id, bytes([service_id]))
+    if team_net_no > 65535:
+        raise ValueError('Team net number must fit in 16 bits')
+    protected_data = struct.pack("!i H c", int(timestamp), team_net_no, bytes([service_id]))
 
     if payload is None:
         payload = struct.pack("!I I", zlib.crc32(protected_data), 0)

commit 77389e3f0f02fff4e0faa45d4fa4860879ba9ce7

Submission: fix submission of flags
Broken in e2fabb5 ("Introduce net numbers to make teams more anonymous",
2020-06-20)

src/ctf_gameserver/submission/flagserver.py
@@ -23,14 +23,14 @@ class FlagHandler(asynchat.async_chat):
             self.close()
             return
 
-        self.team = int(match.group(1))
+        self.capturing_team = int(match.group(1))
         self.set_terminator(b"\n")
         self._logger = logging.getLogger("%13s %5d" % (ipaddr, port))
         self._cursor = None
         self._dbconnection = dbconnection
         self._secret = base64.b64decode(secret)
         self.buffer = b''
-        self._logger.info("Accepted connection from Team (Net Number) %s", self.team)
+        self._logger.info("Accepted connection from Team (Net Number) %s", self.capturing_team)
         self._banner()
         self._conteststart = conteststart
         self._contestend = contestend
@@ -72,7 +72,7 @@ class FlagHandler(asynchat.async_chat):
             return
 
         try:
-            team, service, _, timestamp = flag.verify(curflag, self._secret)
+            protecting_team, service, _, timestamp = flag.verify(curflag, self._secret)
         except flag.InvalidFlagFormat:
             self._reply(b"Flag not recognized")
             return
@@ -83,12 +83,12 @@ class FlagHandler(asynchat.async_chat):
             self._reply((u"Flag expired since %.1f seconds" % e.args).encode('utf-8'))
             return
 
-        if team == self.team:
+        if protecting_team == self.capturing_team:
             self._reply(b"Can't submit a flag for your own team")
             return
 
         try:
-            result = self._store_capture(team, service, timestamp)
+            result = self._store_capture(protecting_team, service, timestamp)
             if result:
                 self._reply(u"Thank you for your submission!".encode('utf-8'))
 
@@ -99,19 +99,27 @@ class FlagHandler(asynchat.async_chat):
             self._reply(u"Something went wrong with your submission!".encode('utf-8'))
 
 
-    def _store_capture(self, team, service, timestamp):
+    def _store_capture(self, protecting_team, service, timestamp):
         with self._dbconnection:
             with self._dbconnection.cursor() as cursor:
                 cursor.execute("""SELECT user_id FROM registration_team WHERE net_number = %s""",
-                               (team,))
+                               (protecting_team,))
                 data = cursor.fetchone()
                 if data is None:
                     self._reply(u"Unknown team net".encode("utf-8"))
                     return False
-                team = data[0]
+                protecting_team_id = data[0]
+
+                cursor.execute("""SELECT user_id FROM registration_team WHERE net_number = %s""",
+                               (self.capturing_team,))
+                data = cursor.fetchone()
+                if data is None:
+                    self._reply(u"Unknown team net".encode("utf-8"))
+                    return False
+                capturing_team_id = data[0]
 
                 cursor.execute("""SELECT nop_team FROM registration_team WHERE user_id = %s""",
-                               (team,))
+                               (protecting_team_id,))
                 nopp, = cursor.fetchone()
                 if nopp:
                     self._reply(u"Can not submit flags for the NOP team".encode("utf-8"))
@@ -122,13 +130,13 @@ class FlagHandler(asynchat.async_chat):
                                   WHERE service_id = %s
                                     AND protecting_team_id = %s
                                     AND tick = %s""",
-                               (service, team, tick))
+                               (service, protecting_team_id, tick))
                 flag_id = cursor.fetchone()[0]
 
                 cursor.execute("""SELECT count(*) FROM scoring_capture
                                   WHERE flag_id = %s
                                     AND capturing_team_id = %s""",
-                               (flag_id, self.team))
+                               (flag_id, capturing_team_id))
                 count = cursor.fetchone()[0]
 
                 if count > 0:
@@ -141,7 +149,7 @@ class FlagHandler(asynchat.async_chat):
                                       (%s, %s, now(),
                                        (SELECT current_tick
                                         FROM scoring_gamecontrol))""",
-                               (flag_id, self.team))
+                               (flag_id, capturing_team_id))
                 return True
 
 