fausecteam/ctf-gameserver

commit 22effe62fa10519a7fd88fc4e6edfc10b4d5ec27

Web: Block changes during updates in (internal) Service History
Prevent multiple ongoing updates by making all fields read-only and
showing a progress spinner during updates.

"progress_spinner.gif" is "Indicator Big 2" from
http://www.ajaxload.info.
As per the site's footer, "[g]enerated gifs can be used, modified and
distributed under the terms of the Do What The Fuck You Want To Public
License" (http://www.wtfpl.net).

Closes: https://github.com/fausecteam/ctf-gameserver/issues/56

src/ctf_gameserver/web/static/service_history.js
@@ -22,6 +22,9 @@ $(document).ready(function() {
 
 function loadTable(_, ignoreMaxTick=false) {
 
+    makeFieldsEditable(false)
+    $('#load-spinner').attr('hidden', false)
+
     const serviceSlug = window.location.hash.slice(1)
     if (serviceSlug.length == 0) {
         return
@@ -37,7 +40,22 @@ function loadTable(_, ignoreMaxTick=false) {
     if (!ignoreMaxTick) {
         params['to-tick'] = toTick
     }
-    $.getJSON('service-history.json', params, buildTable)
+    $.getJSON('service-history.json', params, function(data) {
+        buildTable(data)
+        $('#load-spinner').attr('hidden', true)
+        makeFieldsEditable(true)
+    })
+
+}
+
+
+function makeFieldsEditable(writeable) {
+
+    $('#service-selector').attr('disabled', !writeable)
+    $('#min-tick').attr('readonly', !writeable)
+    $('#max-tick').attr('readonly', !writeable)
+    $('#refresh').attr('disabled', !writeable)
+    $('#load-current').attr('disabled', !writeable)
 
 }
 

setup.py
@@ -57,6 +57,7 @@ setup(
             'templates/*.txt',
             'static/robots.txt',
             'static/*.css',
+            'static/*.gif',
             'static/*.js',
             'static/ext/jquery.min.js',
             'static/ext/bootstrap/css/*',

src/ctf_gameserver/web/scoring/templates/service_history.html
@@ -16,7 +16,7 @@
 
 <div class="clearfix">
     <div class="dropdown pull-left">
-        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+        <button type="button" id="service-selector" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
             <span id="selected-service">{% trans 'Select service' %}</span> <span class="caret"></span>
         </button>
         <ul class="dropdown-menu">
@@ -27,11 +27,12 @@
     </div>
 
     <div class="form-inline pull-right">
-        <button type="button" id="refresh" class="btn btn-default pull-left">
+        <img src="{% static 'progress_spinner.gif' %}" id="load-spinner" hidden="hidden" alt="Progress spinner" />
+        <button type="button" id="refresh" class="btn btn-default">
             <span class="glyphicon glyphicon-refresh"></span>
         </button>
 
-        <fieldset>
+        <fieldset class="pull-right">
             <label>Tick:</label>
             <div class="input-group">
                 <div class="input-group-addon">{% trans 'Min' %}</div>

src/ctf_gameserver/web/static/style.css
@@ -68,8 +68,12 @@ th.border-right, td.border-right {
     display: block;
 }
 
-button#refresh {
-    margin-right: 25px;
+img#load-spinner {
+    width: 1.7em;
+}
+
+img#load-spinner, button#refresh {
+    margin-right: 20px;
 }
 
 #history-table td a:hover {

commit 28b4828d087931b8c0966b8cb0aa5ccab34deaaa

Web: Permit selecting a service in (internal) Service History
All fields were disabled when loading the Service History with no
service selected. This prevented the user from selecting a service.

Fixes: 22effe6 (Web: Block changes during updates in (internal) Service History, 2020-12-29)

src/ctf_gameserver/web/static/service_history.js
@@ -27,6 +27,8 @@ function loadTable(_, ignoreMaxTick=false) {
 
     const serviceSlug = window.location.hash.slice(1)
     if (serviceSlug.length == 0) {
+        $('#load-spinner').attr('hidden', true)
+        makeFieldsEditable(true)
         return
     }
 