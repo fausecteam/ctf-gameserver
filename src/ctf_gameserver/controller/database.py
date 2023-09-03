fausecteam/ctf-gameserver

commit 739fc6f2883fe533ea985f6b364ac86f64c29d1d

Checker: Terminate Checker Scripts after competition has finished
Closes: https://github.com/fausecteam/ctf-gameserver/issues/66

Co-authored-by: Simon Ruderich <simon@ruderich.org>

src/ctf_gameserver/controller/database.py
@@ -27,13 +27,19 @@ def get_control_info(db_conn, prohibit_changes=False):
 def increase_tick(db_conn, prohibit_changes=False):
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
-        cursor.execute('UPDATE scoring_gamecontrol SET current_tick = current_tick + 1')
+        cursor.execute('UPDATE scoring_gamecontrol SET current_tick = current_tick + 1, cancel_checks = 0')
         # Create flags for every service and team in the new tick
         cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                        '    SELECT service.id, team.user_id, control.current_tick'
                        '    FROM scoring_service service, auth_user, registration_team team,'
                        '         scoring_gamecontrol control'
                        '    WHERE auth_user.id = team.user_id AND auth_user.is_active')
+
+
+def cancel_checks(db_conn, prohibit_changes=False):
+
+    with transaction_cursor(db_conn, prohibit_changes) as cursor:
+        cursor.execute('UPDATE scoring_gamecontrol SET cancel_checks = 1')
 
 
 def update_scoring(db_conn):

tests/checker/test_integration.py
@@ -237,6 +237,63 @@ class IntegrationTest(DatabaseTestCase):
         del os.environ['CHECKERSCRIPT_PIDFILE']
         checkerscript_pidfile.close()
 
+    @patch('logging.warning')
+    @patch('ctf_gameserver.checker.master.get_monotonic_time')
+    def test_cancel_checks(self, monotonic_mock, warning_mock):
+        checkerscript_path = os.path.join(os.path.dirname(__file__),
+                                          'integration_unfinished_checkerscript.py')
+
+        checkerscript_pidfile = tempfile.NamedTemporaryFile()
+        os.environ['CHECKERSCRIPT_PIDFILE'] = checkerscript_pidfile.name
+
+        monotonic_mock.return_value = 10
+        master_loop = MasterLoop(self.connection, 'service1', checkerscript_path, None, 2, 1, 10,
+                                 '0.0.%s.1', b'secret', {}, DummyQueue())
+
+        with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET start=NOW()')
+            cursor.execute('UPDATE scoring_gamecontrol SET current_tick=0')
+            cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
+                           '    VALUES (1, 2, 0)')
+        monotonic_mock.return_value = 20
+
+        master_loop.supervisor.queue_timeout = 0.01
+        # Checker Script gets started, will return False because no messages yet
+        self.assertFalse(master_loop.step())
+        master_loop.supervisor.queue_timeout = 10
+        self.assertTrue(master_loop.step())
+
+        with transaction_cursor(self.connection) as cursor:
+            cursor.execute('SELECT data FROM scoring_checkerstate WHERE service_id=1 AND team_id=2')
+            state_result = cursor.fetchone()
+        self.assertEqual(state_result[0], 'gASVHgAAAAAAAACMGkxvcmVtIGlwc3VtIGRvbG9yIHNpdCBhbWV0lC4=')
+
+        checkerscript_pidfile.seek(0)
+        checkerscript_pid = int(checkerscript_pidfile.read())
+        # Ensure process is running by sending signal 0
+        os.kill(checkerscript_pid, 0)
+
+        with transaction_cursor(self.connection) as cursor:
+            cursor.execute('UPDATE scoring_gamecontrol SET cancel_checks=1')
+
+        master_loop.supervisor.queue_timeout = 0.01
+        monotonic_mock.return_value = 190
+        self.assertFalse(master_loop.step())
+        # Poll whether the process has been killed
+        for _ in range(100):
+            try:
+                os.kill(checkerscript_pid, 0)
+            except ProcessLookupError:
+                break
+            time.sleep(0.1)
+        with self.assertRaises(ProcessLookupError):
+            os.kill(checkerscript_pid, 0)
+
+        warning_mock.assert_called_with('Terminating all %d Runner processes', 1)
+
+        del os.environ['CHECKERSCRIPT_PIDFILE']
+        checkerscript_pidfile.close()
+
     @patch('ctf_gameserver.checker.master.get_monotonic_time')
     def test_multi_teams_ticks(self, monotonic_mock):
         checkerscript_path = os.path.join(os.path.dirname(__file__),

tests/controller/test_main_loop.py
@@ -55,6 +55,11 @@ class MainLoopTest(DatabaseTestCase):
         self.assertEqual(new_tick, 0)
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('SELECT cancel_checks FROM scoring_gamecontrol')
+            cancel_checks = cursor.fetchone()[0]
+        self.assertFalse(cancel_checks)
+
+        with transaction_cursor(self.connection) as cursor:
             cursor.execute('SELECT COUNT(*) FROM scoring_flag')
             total_flag_count = cursor.fetchone()[0]
         self.assertEqual(total_flag_count, 6)
@@ -101,9 +106,9 @@ class MainLoopTest(DatabaseTestCase):
     @patch('time.sleep')
     def test_next_tick_overdue(self, sleep_mock, _):
         with transaction_cursor(self.connection) as cursor:
-            cursor.execute('UPDATE scoring_gamecontrol SET start = datetime("now", "-19 minutes"), '
-                           '                               end = datetime("now", "+1421 minutes"), '
-                           '                               current_tick=5')
+            cursor.execute('UPDATE scoring_gamecontrol SET start=datetime("now", "-19 minutes"), '
+                           '                               end=datetime("now", "+1421 minutes"), '
+                           '                               current_tick=5, cancel_checks=1')
 
         controller.main_loop_step(self.connection, self.metrics, False)
 
@@ -113,6 +118,11 @@ class MainLoopTest(DatabaseTestCase):
             cursor.execute('SELECT current_tick FROM scoring_gamecontrol')
             new_tick = cursor.fetchone()[0]
         self.assertEqual(new_tick, 6)
+
+        with transaction_cursor(self.connection) as cursor:
+            cursor.execute('SELECT cancel_checks FROM scoring_gamecontrol')
+            cancel_checks = cursor.fetchone()[0]
+        self.assertFalse(cancel_checks)
 
         with transaction_cursor(self.connection) as cursor:
             cursor.execute('SELECT COUNT(*) FROM scoring_flag WHERE tick=6')
@@ -157,6 +167,11 @@ class MainLoopTest(DatabaseTestCase):
         self.assertEqual(new_tick, 479)
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('SELECT cancel_checks FROM scoring_gamecontrol')
+            cancel_checks = cursor.fetchone()[0]
+        self.assertTrue(cancel_checks)
+
+        with transaction_cursor(self.connection) as cursor:
             cursor.execute('SELECT COUNT(*) FROM scoring_flag')
             total_flag_count = cursor.fetchone()[0]
         self.assertEqual(total_flag_count, 0)
@@ -179,6 +194,11 @@ class MainLoopTest(DatabaseTestCase):
         self.assertEqual(new_tick, 479)
 
         with transaction_cursor(self.connection) as cursor:
+            cursor.execute('SELECT cancel_checks FROM scoring_gamecontrol')
+            cancel_checks = cursor.fetchone()[0]
+        self.assertTrue(cancel_checks)
+
+        with transaction_cursor(self.connection) as cursor:
             cursor.execute('SELECT COUNT(*) FROM scoring_flag')
             total_flag_count = cursor.fetchone()[0]
         self.assertEqual(total_flag_count, 0)
@@ -197,6 +217,11 @@ class MainLoopTest(DatabaseTestCase):
             cursor.execute('SELECT current_tick FROM scoring_gamecontrol')
             new_tick = cursor.fetchone()[0]
         self.assertEqual(new_tick, 480)
+
+        with transaction_cursor(self.connection) as cursor:
+            cursor.execute('SELECT cancel_checks FROM scoring_gamecontrol')
+            cancel_checks = cursor.fetchone()[0]
+        self.assertFalse(cancel_checks)
 
         with transaction_cursor(self.connection) as cursor:
             cursor.execute('SELECT COUNT(*) FROM scoring_flag WHERE tick=480')

src/ctf_gameserver/checker/database.py
@@ -44,17 +44,17 @@ def get_service_attributes(db_conn, service_slug, prohibit_changes=False):
 
 def get_current_tick(db_conn, prohibit_changes=False):
     """
-    Reads the current tick from the game database.
+    Reads the current tick and the "cancel_checks" field from the game database.
     """
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
-        cursor.execute('SELECT current_tick FROM scoring_gamecontrol')
+        cursor.execute('SELECT current_tick, cancel_checks FROM scoring_gamecontrol')
         result = cursor.fetchone()
 
     if result is None:
         raise DBDataError('Game control information has not been configured')
 
-    return result[0]
+    return result
 
 
 def get_check_duration(db_conn, service_id, std_dev_count, prohibit_changes=False):

src/ctf_gameserver/checker/master.py
@@ -336,12 +336,16 @@ class MasterLoop:
             self.update_launch_params(new_tick)
             self.known_tick = new_tick
 
-        current_tick = database.get_current_tick(self.db_conn)
+        current_tick, cancel_checks = database.get_current_tick(self.db_conn)
         if current_tick < 0:
             # Competition not running yet
             return
         if current_tick != self.known_tick:
             change_tick(current_tick)
+        elif cancel_checks:
+            # Competition over
+            self.supervisor.terminate_runners()
+            return
 
         tasks = database.get_new_tasks(self.db_conn, self.service['id'], self.tasks_per_launch)
 

src/ctf_gameserver/controller/controller.py
@@ -181,6 +181,8 @@ def main_loop_step(db_conn, metrics, nonstop):
         return
 
     if (not nonstop) and (now >= control_info['end']):
+        database.cancel_checks(db_conn)
+
         # Update scoring for last tick of game
         database.update_scoring(db_conn)
 

src/ctf_gameserver/web/scoring/forms.py
@@ -16,7 +16,7 @@ class GameControlAdminForm(forms.ModelForm):
 
     class Meta:
         model = models.GameControl
-        exclude = ('current_tick',)
+        exclude = ('current_tick', 'cancel_checks')
         help_texts = {
             'competition_name': _('Human-readable title of the CTF'),
             'services_public': _('Time at which information about the services is public, but the actual '

src/ctf_gameserver/web/scoring/models.py
@@ -155,6 +155,8 @@ class GameControl(models.Model):
     # Number of ticks a flag is valid for including the one it was generated in
     valid_ticks = models.PositiveSmallIntegerField(default=5)
     current_tick = models.IntegerField(default=-1)
+    # Instruct Checker Runners to cancel any running checks
+    cancel_checks = models.BooleanField(default=False)
     flag_prefix = models.CharField(max_length=20, default='FLAG_')
     registration_open = models.BooleanField(default=False)
     registration_confirm_text = models.TextField(blank=True)

commit 54ded1fbd74d2ccb41c72ee4f7be619cfb74e9e1

Checker: Assign boolean values to cancel_checks
SQLite works fine with integers but Postgres only permits boolean
values.

Fixes: 739fc6f (Checker: Terminate Checker Scripts after competition has finished, 2023-04-22)

src/ctf_gameserver/controller/database.py
@@ -27,7 +27,8 @@ def get_control_info(db_conn, prohibit_changes=False):
 def increase_tick(db_conn, prohibit_changes=False):
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
-        cursor.execute('UPDATE scoring_gamecontrol SET current_tick = current_tick + 1, cancel_checks = 0')
+        cursor.execute('UPDATE scoring_gamecontrol SET current_tick = current_tick + 1,'
+                       '                               cancel_checks = false')
         # Create flags for every service and team in the new tick
         cursor.execute('INSERT INTO scoring_flag (service_id, protecting_team_id, tick)'
                        '    SELECT service.id, team.user_id, control.current_tick'
@@ -39,7 +40,7 @@ def increase_tick(db_conn, prohibit_changes=False):
 def cancel_checks(db_conn, prohibit_changes=False):
 
     with transaction_cursor(db_conn, prohibit_changes) as cursor:
-        cursor.execute('UPDATE scoring_gamecontrol SET cancel_checks = 1')
+        cursor.execute('UPDATE scoring_gamecontrol SET cancel_checks = true')
 
 
 def update_scoring(db_conn):

tests/checker/test_integration.py
@@ -274,7 +274,7 @@ class IntegrationTest(DatabaseTestCase):
         os.kill(checkerscript_pid, 0)
 
         with transaction_cursor(self.connection) as cursor:
-            cursor.execute('UPDATE scoring_gamecontrol SET cancel_checks=1')
+            cursor.execute('UPDATE scoring_gamecontrol SET cancel_checks=true')
 
         master_loop.supervisor.queue_timeout = 0.01
         monotonic_mock.return_value = 190

tests/controller/test_main_loop.py
@@ -108,7 +108,7 @@ class MainLoopTest(DatabaseTestCase):
         with transaction_cursor(self.connection) as cursor:
             cursor.execute('UPDATE scoring_gamecontrol SET start=datetime("now", "-19 minutes"), '
                            '                               end=datetime("now", "+1421 minutes"), '
-                           '                               current_tick=5, cancel_checks=1')
+                           '                               current_tick=5, cancel_checks=true')
 
         controller.main_loop_step(self.connection, self.metrics, False)
 