fausecteam/ctf-gameserver

commit c4d3c12b8e9d242029277087c1296043933ae5a0

Web: Convert scoreboard and service status to client-side rendering
This will hopefully improve performance.

Co-authored-by: Simon Ruderich <simon@ruderich.org>
Closes: https://github.com/fausecteam/ctf-gameserver/issues/24

src/ctf_gameserver/web/static/service_status.js
@@ -1,0 +1,81 @@
+/* jshint asi: true, sub: true */
+
+'use strict'
+
+
+$(document).ready(function() {
+    $.getJSON('../status.json', {}, buildTable)
+})
+
+function buildTable(data) {
+    const statusDescriptions = data['status-descriptions']
+
+    // Extract raw DOM element from jQuery object
+    const table = $('#status-table')[0]
+
+    // Tick header
+    const tick_template = table.querySelectorAll('thead th')[2]
+    for (const tick of data['ticks']) {
+        const node = tick_template.cloneNode(true)
+        node.querySelector('span').textContent = tick
+        tick_template.parentNode.insertBefore(node, tick_template)
+    }
+    tick_template.parentNode.removeChild(tick_template)
+
+    // Do not use jQuery here for performance reasons (we're creating a lot of elements)
+    const template = $('#team-template-row')[0]
+    for (const team of data['teams']) {
+        const entry = template.cloneNode(true)
+        entry.setAttribute('id', `team-${team.id}-row`)
+        const tds = entry.querySelectorAll('td')
+        if (!team.nop) {
+            entry.setAttribute('class', '')
+        }
+
+        // image
+        if (team['image'] === undefined) {
+            tds[0].innerHTML = '' // delete all children
+        } else {
+            tds[0].querySelector('a').setAttribute('href', team['image'])
+            const img = tds[0].querySelector('img')
+            img.setAttribute('src', team['thumbnail'])
+            img.setAttribute('alt', team['name'])
+        }
+
+        // Name
+        tds[1].querySelector('strong').textContent = team['name']
+
+        // Service status per tick
+        for (const statuses of team['ticks']) {
+            const col = document.createElement('td')
+
+            for (let i = 0; i < statuses.length; i++) {
+                let status = statuses[i]
+
+                const text = document.createTextNode(data['services'][i] + ': ')
+
+                const span = document.createElement('span')
+                let statusClass
+                if (status === '') {
+                    // Not checked
+                    status = -1    // For `statusDescriptions`
+                    statusClass = 'muted'
+                } else {
+                    statusClass = statusClasses[status]
+                }
+                span.setAttribute('class', `text-${statusClass}`)
+                span.textContent = statusDescriptions[status]
+
+                col.appendChild(text)
+                col.appendChild(span)
+                col.appendChild(document.createElement('br'))
+            }
+
+            entry.appendChild(col)
+        }
+        template.parentNode.appendChild(entry)
+    }
+    template.parentNode.removeChild(template)
+
+    table.hidden = false
+}

src/ctf_gameserver/web/scoring/calculations.py
@@ -74,7 +74,7 @@ def team_statuses(from_tick, to_tick):
                 statuses[team][tick][service] = ''
 
         for check in status_checks.filter(team=team):
-            statuses[team][check.tick][check.service] = check.get_status_display()
+            statuses[team][check.tick][check.service] = check.status
 
     sorted_statuses = OrderedDict(sorted(statuses.items(), key=lambda s: s[0].user.username))
     cache.set(cache_key, sorted_statuses, 10)

src/ctf_gameserver/web/scoring/templates/scoreboard.html
@@ -3,8 +3,16 @@
 {% load cache %}
 {% load dict_access %}
 {% load status_css_class %}
+{% load static %}
 
 {% block content %}
+<script src="{% static 'service_util.js' %}" defer="defer"></script>
+<script src="{% static 'scoreboard.js' %}" defer="defer"></script>
+
+<noscript>
+    <div class="alert alert-danger" role="alert">{% trans 'Please enable JavaScript for this page to work!' %}</div>
+</noscript>
+
 <div class="page-header">
     <h1>{% block title %}{% trans 'Scoreboard' %}{% endblock %}</h1>
 </div>
@@ -16,13 +24,11 @@
 
     <p class="text-muted pull-right">
         {% blocktrans %}
-        Tick: {{ tick }}
+        Tick: <span id="tick"></span>
         {% endblocktrans %}
     </p>
 </div>
 
-{% get_current_language as LANGUAGE_CODE %}
-{% cache 10 html_scoreboard LANGUAGE_CODE %}
 <div class="table-responsive">
     <table class="table">
         <thead>
@@ -50,52 +56,41 @@
         </thead>
 
         <tbody>
-            {% for team, points in scores.items %}
-            <tr id="team-{{ team.user.pk }}-row">
-                <td><strong>{{ forloop.counter }}.</strong></td>
+            <tr id="team-template-row" hidden="hidden">
+                <td><strong><!-- Position --></strong></td>
 
                 <td>
-                    {% if team.image %}
-                    <a href="{{ team.image.url }}">
-                        <img class="team-image" src="{{ team.image.get_thumbnail_url }}"
-                         alt="{{ team.user.username }}" />
+                    <!-- Image -->
+                    <a href="">
+                        <img class="team-image" src="" alt="" />
                     </a>
-                    {% endif %}
                 </td>
 
                 <td class="border-right">
-                    <strong>{{ team.user.username }}</strong>
+                    <strong><!-- Name --></strong>
                 </td>
 
-                {% for service in services %}
-                {% with status=statuses|dict_access:team|dict_access:tick|dict_access:service %}
-                <td class="{{ status|status_css_class }}">
+                <!-- Services, copied via JavaScript -->
+                <td class="active">
                     <span class="glyphicon glyphicon-fire"></span> <span class="sr-only">Offense</span>
-                    {{ points.offense.0|dict_access:service|floatformat:2 }} <br/>
+                    <span><!-- Offense --></span> <br/>
                     <span class="glyphicon glyphicon-tower"></span> <span class="sr-only">Defense</span>
-                    {{ points.defense.0|dict_access:service|floatformat:2 }} <br/>
+                    <span><!-- Defense --></span> <br/>
                     <span class="glyphicon glyphicon-dashboard"></span> <span class="sr-only">SLA</span>
-                    {{ points.sla.0|dict_access:service|floatformat:2 }} <br/>
+                    <span><!-- SLA --></span> <br/>
 
-                    <a href="{% url 'service_status' %}#team-{{ team.user.id }}-row">
-                    {% if status %}
-                    <span class="text-{{ status|status_css_class }}">{{ status }}</span>
-                    {% else %}
+                    <a href="{% url 'service_status' %}">
+                    <!-- Status -->
                     <span class="text-muted">{% trans 'not checked' %}</span>
-                    {% endif %}
                     </a>
                 </td>
-                {% endwith %}
-                {% endfor %}
 
-                <td class="border-left">{{ points.offense.1|floatformat:2 }}</td>
-                <td>{{ points.defense.1|floatformat:2 }}</td>
-                <td>{{ points.sla.1|floatformat:2 }}</td>
-                <td class="border-left"><strong>{{ points.total|floatformat:2 }}</strong></td>
+                <td class="border-left"><!-- Offense --></td>
+                <td><!-- Defense --></td>
+                <td><!-- SLA --></td>
+                <td class="border-left"><strong><!-- Total --></strong></td>
             </tr>
-            {% endfor %}
         </tbody>
     </table>
 </div>
-{% endcache %}
 {% endblock %}

src/ctf_gameserver/web/scoring/templates/service_status.html
@@ -3,64 +3,50 @@
 {% load cache %}
 {% load dict_access %}
 {% load status_css_class %}
+{% load static %}
 
 {% block content %}
+<script src="{% static 'service_util.js' %}" defer="defer"></script>
+<script src="{% static 'service_status.js' %}" defer="defer"></script>
+
+<noscript>
+    <div class="alert alert-danger" role="alert">{% trans 'Please enable JavaScript for this page to work!' %}</div>
+</noscript>
+
 <div class="page-header">
     <h1>{% block title %}{% trans 'Service status' %}{% endblock %}</h1>
 </div>
 
 {% include 'competition_nav.html' with active='service_status' %}
 
-{% get_current_language as LANGUAGE_CODE %}
-{% cache 10 html_status LANGUAGE_CODE %}
 <div class="table-responsive">
-    <table class="table">
+    <table id="status-table" class="table" hidden="hidden">
         <thead>
             <tr>
                 <th class="image-column"><!-- Image --></th>
                 <th class="border-right">{% trans 'Team' %}</th>
-                {% for tick in ticks %}
-                <th>Tick {{ tick }}</th>
-                {% endfor %}
+                <!-- Ticks, copied via JavaScript -->
+                <th>Tick <span><!-- Tick --></span></th>
             </tr>
         </thead>
 
         <tbody>
-            {% for team, tick_statuses in statuses.items %}
-            <tr id="team-{{ team.user.pk }}-row" class="{% if team.nop_team %}active{% endif %}">
+            <!-- Teams, copied via JavaScript -->
+            <tr id="team-template-row" class="active">
                 <td>
-                    {% if team.image %}
-                    <a href="{{ team.image.url }}">
-                        <img class="team-image" src="{{ team.image.get_thumbnail_url }}"
-                         alt="{{ team.user.username }}" />
+                    <!-- Image -->
+                    <a href="">
+                        <img class="team-image" src="" alt="" />
                     </a>
-                    {% endif %}
                 </td>
 
                 <td class="border-right">
-                    <strong>{{ team.user.username }}</strong>
+                    <strong><!-- Name --></strong>
                 </td>
 
-                {% for tick in ticks %}
-                <td>
-                    {% for service in services %}
-                    {{ service.name }}:
-
-                    {% with status=tick_statuses|dict_access:tick|dict_access:service %}
-                    {% if status %}
-                    <span class="text-{{ status|status_css_class }}">{{ status }}</span> <br/>
-                    {% else %}
-                    <span class="text-muted">{% trans 'not checked' %}</span> <br/>
-                    {% endif %}
-                    {% endwith %}
-
-                    {% endfor %}
-                </td>
-                {% endfor %}
+                <!-- JavaScript adds service status for ticks -->
             </tr>
-            {% endfor %}
         </tbody>
     </table>
 </div>
-{% endcache %}
 {% endblock %}

src/ctf_gameserver/web/scoring/views.py
@@ -14,6 +14,16 @@ from .decorators import competition_started_required
 @competition_started_required
 def scoreboard(request):
 
+    return render(request, 'scoreboard.html', {
+        'services': models.Service.objects.all()
+    })
+
+
+# Short cache timeout only, because there is already caching going on in calculations
+@cache_page(5)
+@competition_started_required
+def scoreboard_json(_):
+
     game_control = models.GameControl.get_instance()
 
     if game_control.competition_over():
@@ -23,16 +33,55 @@ def scoreboard(request):
 
     scores = calculations.scores()
     statuses = calculations.team_statuses(to_tick, to_tick)
+    services = models.Service.objects.all()
 
-    return render(request, 'scoreboard.html', {
-        'scores': scores,
-        'services': models.Service.objects.all(),
-        'statuses': statuses,
-        'tick': to_tick
-    })
+    response = {
+        'tick': to_tick,
+        'teams': [],
+        'status-descriptions': _get_status_descriptions()
+    }
+
+    for rank, (team, points) in enumerate(scores.items(), start=1):
+        team_entry = {
+            'rank': rank,
+            'id': team.user.pk,
+            'name': team.user.username,
+            'services': [],
+            'offense': points['offense'][1],
+            'defense': points['defense'][1],
+            'sla': points['sla'][1],
+            'total': points['total'],
+        }
+        if team.image:
+            team['image'] = team.image.url
+            team['thumbnail'] = team.image.get_thumbnail_url
+
+        for service in services:
+            try:
+                offense = points['offense'][0][service]
+                defense = points['defense'][0][service]
+                sla = points['sla'][0][service]
+            except KeyError:
+                offense = 0
+                defense = 0
+                sla = 0
+            try:
+                status = statuses[team][to_tick][service]
+            except KeyError:
+                status = ''
+            team_entry['services'].append({
+                'status': status,
+                'offense': offense,
+                'defense': defense,
+                'sla': sla
+            })
+
+        response['teams'].append(team_entry)
+
+    return JsonResponse(response)
 
 
-def scoreboard_json(_):
+def scoreboard_json_ctftime(_):
     """
     View which returns the scoreboard in CTFTime scoreboard feed format,
     see https://ctftime.org/json-scoreboard-feed.
@@ -65,6 +114,14 @@ def scoreboard_json(_):
 @competition_started_required
 def service_status(request):
 
+    return render(request, 'service_status.html')
+
+
+# Short cache timeout only, because there is already caching going on in calculations
+@cache_page(5)
+@competition_started_required
+def service_status_json(_):
+
     game_control = models.GameControl.get_instance()
     to_tick = game_control.current_tick
     from_tick = to_tick - 4
@@ -73,12 +130,41 @@ def service_status(request):
         from_tick = 0
 
     statuses = calculations.team_statuses(from_tick, to_tick)
+    services = models.Service.objects.all().order_by('name')
 
-    return render(request, 'service_status.html', {
-        'statuses': statuses,
-        'ticks': range(from_tick, to_tick+1),
-        'services': models.Service.objects.all().order_by('name')
-    })
+    response = {
+        'ticks': list(range(from_tick, to_tick+1)),
+        'teams': [],
+        'services': [],
+        'status-descriptions': _get_status_descriptions()
+    }
+
+    for team, tick_statuses in statuses.items():
+        team_entry = {
+            'id': team.user.pk,
+            'nop': team.nop_team,
+            'name': team.user.username,
+            'ticks': [],
+        }
+        if team.image:
+            team['image'] = team.image.url
+            team['thumbnail'] = team.image.get_thumbnail_url
+
+        for tick in response['ticks']:
+            tick_services = []
+            for service in services:
+                try:
+                    tick_services.append(tick_statuses[tick][service])
+                except KeyError:
+                    tick_services.append('')
+            team_entry['ticks'].append(tick_services)
+
+        response['teams'].append(team_entry)
+
+    for service in services:
+        response['services'].append(service.name)
+
+    return JsonResponse(response)
 
 
 @cache_page(60)

src/ctf_gameserver/web/static/scoreboard.js
@@ -1,0 +1,77 @@
+/* jshint asi: true, sub: true */
+
+'use strict'
+
+
+$(document).ready(function() {
+    $.getJSON('../scoreboard.json', {}, buildTable)
+})
+
+function buildTable(data) {
+    const statusDescriptions = data['status-descriptions']
+
+    $('#tick').text(data['tick'])
+
+    // Extract raw DOM element from jQuery object
+    const template = $('#team-template-row')[0]
+
+    // Do not use jQuery here for performance reasons (we're creating a lot of elements)
+    for (const team of data['teams']) {
+        const entry = template.cloneNode(true)
+        entry.setAttribute('id', `team-${team.id}-row`)
+        const tds = entry.querySelectorAll('td')
+
+        // Position
+        tds[0].querySelector('strong').textContent = `${team.rank}.`
+
+        // Image
+        if (team['image'] === undefined) {
+            // Delete all children
+            tds[1].innerHTML = ''
+        } else {
+            tds[1].querySelector('a').setAttribute('href', team['image'])
+            const img = tds[1].querySelector('img')
+            img.setAttribute('src', team['thumbnail'])
+            img.setAttribute('alt', team['name'])
+        }
+
+        // Name
+        tds[2].querySelector('strong').textContent = team['name']
+
+        // Service
+        const service_template = tds[3]
+        for (const service of team['services']) {
+            const service_node = service_template.cloneNode(true)
+
+            const spans = service_node.querySelectorAll('span')
+            spans[2].textContent = service['offense'].toFixed(2)
+            spans[5].textContent = service['defense'].toFixed(2)
+            spans[8].textContent = service['sla'].toFixed(2)
+
+            service_node.querySelector('a').href += `#team-${team.id}-row`
+
+            if (service['status'] !== '') {
+                const statusClass = statusClasses[service['status']]
+                spans[9].setAttribute('class', `text-${statusClass}`)
+                spans[9].textContent = statusDescriptions[service['status']]
+
+                service_node.setAttribute('class', statusClass)
+            }
+
+            service_template.parentNode.insertBefore(service_node, service_template)
+        }
+        service_template.parentNode.removeChild(service_template)
+
+        // Offense
+        tds[4].textContent = team['offense'].toFixed(2)
+        // Defense
+        tds[5].textContent = team['defense'].toFixed(2)
+        // SLA
+        tds[6].textContent = team['sla'].toFixed(2)
+        // Total
+        tds[7].querySelector('strong').textContent = team['total'].toFixed(2)
+
+        template.parentNode.appendChild(entry)
+        entry.hidden = false
+    }
+}

src/ctf_gameserver/web/static/service_history.js
@@ -1,3 +1,5 @@
+/* jshint asi: true, sub: true */
+
 'use strict'
 
 

src/ctf_gameserver/web/urls.py
@@ -87,9 +87,17 @@ urlpatterns = [
         scoring_views.scoreboard_json,
         name='scoreboard_json'
     ),
+    url(r'^competition/scoreboard-ctftime\.json$',
+        scoring_views.scoreboard_json_ctftime,
+        name='scoreboard_json_ctftime'
+    ),
     url(r'^competition/status/$',
         scoring_views.service_status,
         name='service_status'
+    ),
+    url(r'^competition/status\.json$',
+        scoring_views.service_status_json,
+        name='service_status_json'
     ),
 
     url(r'^internal/mail-teams/$',

commit 253ea375a8b168ea09adac2124380a4b9b8c9207

Web: Fix anchor link scrolling from Scoreboard to Service Status
Must have been broken since c4d3c12 (Web: Convert scoreboard and service
status to client-side rendering, 2020-06-26).

Co-authored-by: Simon Ruderich <simon@ruderich.org>

src/ctf_gameserver/web/static/service_status.js
@@ -78,4 +78,11 @@ function buildTable(data) {
     template.parentNode.removeChild(template)
 
     table.hidden = false
+
+    if (location.hash !== '') {
+        const scrollRow = document.querySelector(location.hash)
+        if (scrollRow !== undefined) {
+            scrollRow.scrollIntoView({'behavior': 'instant'})
+        }
+    }
 }